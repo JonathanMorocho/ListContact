156c2785b3f082e4178df9b005abd6271548e07e
JonathanMorocho <jamorocho@tecnologicoloja.edu.ec>
2021-09-10 09:12:45 -0500

    Lista de Contactos

---
+++ app/src/main/java/com/example/listcontactos/Contacto.java
@@ -0,0 +1,59 @@
+package com.example.listcontactos;
+
+public class Contacto {
+
+    private String nombre;
+    private String apellido;
+    private String direccion;
+    private String email;
+    private String telefono;
+    private String url;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+}
